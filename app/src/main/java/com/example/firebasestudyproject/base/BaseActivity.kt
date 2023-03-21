a5e050124fdb1e74196d46484bcecec6421387a8
raj.kayasth <raj.kayasth@brainvire.com>
2023-03-21 19:13:35 +0530

    - firebase auth completed using email password

--- app/src/main/java/com/example/firebasestudyproject/base/BaseActivity.kt
+++ app/src/main/java/com/example/firebasestudyproject/base/BaseActivity.kt
@@ -1,22 +1,40 @@
 package com.example.firebasestudyproject.base
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.firebasestudyproject.R
 import com.google.android.material.snackbar.Snackbar
 
 open class BaseActivity : AppCompatActivity() {
+    private lateinit var mProgressDialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
-        // START
-        /**
-         * A function to show the success and error messages in snack bar component.
-         */
+
 
     }
 
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+        mProgressDialog.setContentView(R.layout.dialog_progess)
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.setCanceledOnTouchOutside(false)
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
+    }
+
+
+    // START
+
+    /**
+     * A function to show the success and error messages in snack bar component.
+     */
     fun showErrorSnackBar(message: String, errorMessage: Boolean) {
         val snackBar =
             Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
